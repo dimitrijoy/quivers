1998993b49867f0fdeebad119dd0c5dedd7922b5
Dimitri Joy <dimitri@Dimitris-MacBook-Air.local>
2020-02-29 21:14:37 -0500

    Additional methods for reducing cyclic quivers implemented

--- Quiver.java
+++ Quiver.java
@@ -5,41 +5,98 @@ import java.util.ArrayList;
  * mutating vertices, and analyzing the results.
  * 
  * @author  Dimitri Joy
- * @version 1.4
+ * @version 1.6
  * created on 2020-01-26
  */
 class Quiver {
     private ArrayList<Arrow> arrows;
-    private ArrayList<Vertex> verticies;
+    private ArrayList<Vertex> vertices;
 
     /**
-     * A Quiver is comprised of a sequence verticies interlinked with
-     * arrows via the Arrow and Vertex classes, respectively.
+     * A quiver comprised of vertices interlinked with
+     * arrows via the Arrow and Vertex classes.
      */
     Quiver() {
+        // instantiates ArrayLists
         arrows = new ArrayList<Arrow>();
-        verticies = new ArrayList<Vertex>();
+        vertices = new ArrayList<Vertex>();
+    }
+
+    /**
+     * Create a copy of an existing quiver.
+     * 
+     * @param q     the quiver to be copied
+     */
+    Quiver(Quiver q) {
+        // instantiates ArrayLists
+        arrows = new ArrayList<Arrow>();
+        vertices = new ArrayList<Vertex>();
+        ArrayList<Vertex> copiedVertices = new ArrayList<Vertex>();
+        for (int i = 0; i < q.arrows.size(); ++i) {
+            Vertex base = q.arrows.get(i).getBase();
+            Vertex terminal = q.arrows.get(i).getTerminal();
+            if (!copiedVertices.contains(base) && !copiedVertices.contains(terminal)) {
+                Arrow arrowCopy = new Arrow(q.arrows.get(i));
+                arrows.add(arrowCopy);
+                vertices.add(arrowCopy.getBase());
+                vertices.add(arrowCopy.getTerminal());
+                copiedVertices.add(arrowCopy.getBase());
+                copiedVertices.add(base);
+                copiedVertices.add(arrowCopy.getTerminal());
+                copiedVertices.add(terminal);
+            } else if (!copiedVertices.contains(base)) {
+                Vertex baseCopy = new Vertex(base);
+                vertices.add(baseCopy);
+                copiedVertices.add(baseCopy);
+                copiedVertices.add(base);
+                for (int j = 0; j < copiedVertices.size(); ++j) {
+                    if (copiedVertices.get(j) == terminal) {
+                        arrows.add(new Arrow(baseCopy, copiedVertices.get(j - 1)));
+                    }
+                }
+            } else if (!copiedVertices.contains(terminal)) {
+                Vertex terminalCopy = new Vertex(terminal);
+                vertices.add(terminalCopy);
+                copiedVertices.add(terminalCopy);
+                copiedVertices.add(terminal);
+                for (int j = 0; j < copiedVertices.size(); ++j) {
+                    if (copiedVertices.get(j) == base) {
+                        arrows.add(new Arrow(copiedVertices.get(j - 1), terminalCopy));
+                    }
+                }
+            } else {
+                Vertex v1 = new Vertex(), v2 = new Vertex();
+                for (int j = 0; j < copiedVertices.size(); ++j) {
+                    if (copiedVertices.get(j) == base) {
+                        v1 = copiedVertices.get(j - 1);
+                    } else if (copiedVertices.get(j) == terminal) {
+                        v2 = copiedVertices.get(j - 1);
+                    }
+                }
+                arrows.add(new Arrow(v1, v2));
+            }
+        }
     }
 
     /**
      * Add an arrow to the quiver.
      * 
-     * @param   a   an arrow to be added
+     * @param a     the arrow to be added
      */
     public void add(Arrow a) {
         arrows.add(a);
         Vertex v1 = a.getBase();
         Vertex v2 = a.getTerminal();
-        if (verticies.isEmpty()) { // only add v1 for the first arrow
-            verticies.add(v1);
+        if (vertices.isEmpty()) { // only add v1 for the first arrow
+            vertices.add(v1);
         }
-        verticies.add(v2);
+        vertices.add(v2);
     }
 
     /**
      * Returns the updated value of the mutated vertex.
      * 
-     * @param   v   the vertex to be updated
+     * @param v     the vertex to be updated
      * 
      * @return      the new value of the vertex
      */
@@ -55,7 +112,10 @@ class Quiver {
             }
         }
         try {
-            val = (arrowsIn + arrowsOut) / v.getValue();
+            if ((arrowsIn + arrowsOut) % v.getValue() == 0)
+                val = (arrowsIn + arrowsOut) / v.getValue();
+            else
+                val = 0;
         } catch(Exception e) { // likely division by 0
             val = 0;
         }
@@ -65,9 +125,10 @@ class Quiver {
     /**
      * Update the value of the mutated vertex.
      * 
-     * @param   v   the vertex to be updated
+     * @param v     the vertex to be updated
      */
     private void updateValue(Vertex v) {
+        int val;
         int arrowsIn = 1, arrowsOut = 1;
         for (int i = 0; i < arrows.size(); ++i) {
             Arrow a = arrows.get(i);
@@ -77,7 +138,14 @@ class Quiver {
                 arrowsOut *= a.getTerminalValue();
             }
         }
-        int val = (arrowsIn + arrowsOut) / v.getValue();
+        try {
+            if ((arrowsIn + arrowsOut) % v.getValue() == 0)
+                val = (arrowsIn + arrowsOut) / v.getValue();
+            else
+                val = 0;
+        } catch(Exception e) { // likely division by 0
+            val = 0;
+        }
         v.setValue(val);
     }
 
@@ -85,7 +153,7 @@ class Quiver {
      * Connect verticies x and y if the relationship x ---> v ---> y
      * exists, where v is the mutated vertex.
      * 
-     * @param   v   the mutated vertex
+     * @param v     the mutated vertex
      */
     private void connect(Vertex v) {
         for (int i = 0; i < arrows.size(); ++i) {
@@ -113,7 +181,7 @@ class Quiver {
     /**
      * Reverse arrows adjacent to the mutated vertex.
      * 
-     * @param   v   the mutated vertex
+     * @param v     the mutated vertex
      */
     private void reverseArrows(Vertex v) {
         for (int i = 0; i < arrows.size(); ++i) {
@@ -142,7 +210,7 @@ class Quiver {
     /**
      * Mutate a vertex in the quiver.
      * 
-     * @param   v   the vertex to be mutated
+     * @param v     the vertex to be mutated
      */
     public void mutate(Vertex v) {
         updateValue(v);
@@ -173,7 +241,7 @@ class Quiver {
     /**
      * Check if every vertex in the quiver shares the same value.
      * 
-     * @param   val the value to be checked
+     * @param val   the value to be checked
      * @return      whether or not the quiver is homogeneous
      */
     public boolean isHomogeneous(int val) {
@@ -196,9 +264,11 @@ class Quiver {
      * @return      whether or not the quiver has an integer assignments that will remains as integers
      */
     public boolean hasValidAssignment() {
-        for (int i = 0; i < verticies.size(); ++i) {
+        Quiver q = new Quiver(this);
+        q.reduceCycles();
+        for (int i = 0; i < q.vertices.size(); ++i) {
             try {
-                if (updatedValue(verticies.get(i)) <= 0)
+                if (updatedValue(q.vertices.get(i)) <= 0)
                     return false; // only positive integers are allowed
             } catch(Exception e) { return false; }
         }
@@ -256,11 +326,13 @@ class Quiver {
     }
 
     /**
-     * Return the mutable verticies of every reducable oriented 3-cycle.
+     * Return the mutable verticies of every reducable open oriented 3-cycle;
+     * that is, each 3-cycle with at least one vertex not attached to
+     * another quiver.
      * 
      * @return      the mutable verticies
      */
-    private ArrayList<Vertex> reducableCycles() {
+    private ArrayList<Vertex> reducableOpenCycles() {
         ArrayList<Vertex> mutations = new ArrayList<Vertex>();
         if (hasOrientedCycles()) {
             ArrayList<Vertex[]> cycles = findOrientedCycles();
@@ -283,12 +355,93 @@ class Quiver {
     }
 
     /**
-     * Reduce every oriented 3-cycle into an acyclic form.
+     * Reduce every open oriented 3-cycle into an acyclic form.
      */
-    public void reduceCycles() {
-        ArrayList<Vertex> mutations = reducableCycles();
+    public void reduceOpenCycles() {
+        ArrayList<Vertex> mutations = reducableOpenCycles();
         for (int i = 0; i < mutations.size(); ++i) {
             mutate(mutations.get(i));
         }
+    }
+
+    /**
+     * Return the mutable verticies of every reducable closed oriented 3-cycle;
+     * that is, each 3-cycle with every vertex attached to another quiver.
+     * 
+     * @return      the mutable verticies
+     */
+    private ArrayList<Vertex> reducableClosedCycles() {
+        ArrayList<Vertex> mutations = new ArrayList<Vertex>();
+        if (hasOrientedCycles()) {
+            ArrayList<Vertex[]> cycles = findOrientedCycles();
+            for (int i = 0; i < cycles.size(); ++i) {
+                Vertex v1 = cycles.get(i)[0]; // only one vertex per 3-cycle is relevant
+                mutations.add(v1);
+                // essentially want to start with one vertex from the 3-cycle
+                // and mutate along the quiver attached to this vertex
+                for (int j = 0; j < arrows.size(); ++j) {
+                    if (arrows.get(j).contains(v1)) { // nested to improve readability
+                        if (!(arrows.get(j).contains(cycles.get(i)[1]) || arrows.get(j).contains(cycles.get(i)[2]))) {
+                            // if-statements are necessary since the direction of each arrow is unpredictable
+                            if (arrows.get(j).getBase() == v1) {
+                                Vertex v2 = arrows.get(j).getTerminal();
+                                mutations.add(v2);
+                                // in a more general case, this method should be defined recursively
+                                // until a vertex with only one arrow is reached
+                                for (int k = 0; k < arrows.size(); ++k) {
+                                    if (arrows.get(k).contains(v2) && !arrows.get(k).contains(v1)) {
+                                        if (arrows.get(k).getBase() == v2) {
+                                            mutations.add(arrows.get(k).getTerminal());
+                                        } else {
+                                            mutations.add(arrows.get(k).getBase());
+                                        }
+                                    }
+                                }
+                            } else {
+                                Vertex v3 = arrows.get(j).getBase();
+                                mutations.add(v3);
+                                for (int k = 0; k < arrows.size(); ++k) {
+                                    if (arrows.get(k).contains(v3) && !arrows.get(k).contains(v1)) {
+                                        if (arrows.get(k).getBase() == v3) {
+                                            mutations.add(arrows.get(k).getTerminal());
+                                        } else {
+                                            mutations.add(arrows.get(k).getBase());
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return mutations;
+    }
+
+    /**
+     * Reduce every closed oriented 3-cycle into an acyclic form.
+     */
+    public void reduceClosedCycles() {
+        ArrayList<Vertex> mutations = reducableClosedCycles();
+        for (int i = 0; i < mutations.size(); ++i) {
+            mutate(mutations.get(i));
+        }
+    }
+
+    /**
+     * Reduce every oriented 3-cycle into an acyclic form.
+     */
+    public void reduceCycles() {
+        reduceOpenCycles(); // order matters
+        reduceClosedCycles();
+    }
+
+    /**
+     * Create a copy of an existing quiver.
+     * 
+     * @return      the copy of an existing quiver
+     */
+    public Quiver copy() {
+        return new Quiver(this);
     }
 }
